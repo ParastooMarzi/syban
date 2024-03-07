102c7ab718c857c22e03fa7c86a25ea419ed2b9d
parastoo marzi <parastoo@gardoon.io>
2024-03-07 19:28:38 +0330

    new4

--- src/state/api.js
+++ src/state/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://localhost:3000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000" }),
   reducerPath: "adminApi",
   tagTypes: [
     "User",
